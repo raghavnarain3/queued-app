0131a6fce91963a0afe9c8d9873b52e50d3eee57
Raghav Narain <raghavnarain3@gmail.com>
2020-04-11 14:14:05 -0400

    updates config

--- queued-app.config.js
+++ queued-app.config.js
@@ -1,35 +1,37 @@
 module.exports = {
-  apps : [{
-    name        : "queued-app-server",
-    script      : "sudo node ./shared-spotify-queue-backend/index.js",
-    watch       : true,
-    env: {
-      "NODE_ENV": "development",
+  apps : [
+    {
+      name: "queued-app-server",
+      script: "sudo node ./shared-spotify-queue-backend/index.js",
+      watch: true,
+      env: {
+        "NODE_ENV": "development",
+      },
+      env_production : {
+       "NODE_ENV": "production"
+      }
     },
-    env_production : {
-     "NODE_ENV": "production"
+    {
+      name: "queued-app-auth",
+      script: "node ./server/index.js",
+      watch: true,
+      env: {
+        "NODE_ENV": "development",
+      },
+      env_production : {
+        "NODE_ENV": "production"
+      }
+    },
+    {
+      name: "queued-app-client",
+      script: "serve -s ./shared-spotify-queue/build -p 3000",
+      watch: true,
+      env: {
+        "NODE_ENV": "development",
+      },
+      env_production: {
+        "NODE_ENV": "production"
+      }
     }
-  },
-  {
-    name       : "queued-app-auth",
-    script     : "node ./server/index.js",
-    watch       : true,
-    env: {
-      "NODE_ENV": "development",
-    },
-    env_production : {
-      "NODE_ENV": "production"
-    }
-  },
-  {
-    name: "queued-app-client",
-    script: "serve -s ./shared-spotify-queue/build -p 3000",
-    watch: true,
-    env: {
-      "NODE_ENV": "development",
-    },
-    env_production: {
-      "NODE_ENV": "production"
-    }   
-  }]
+  ]
 }
