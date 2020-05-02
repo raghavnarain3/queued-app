d40bb478583a15dec6f34a52ac282c575b7679d7
Raghav Narain <raghavnarain3@gmail.com>
2020-05-02 19:55:20 -0400

    adds player controls

--- shared-spotify-queue-backend/index.js
+++ shared-spotify-queue-backend/index.js
@@ -59,6 +59,79 @@ io.on('connection', function (socket) {
     io.in(room).emit('queue', {currently_playing: {}, queue: []});
   });
 
+  socket.on('pause', function (message) {
+    room = message["room"]
+    if (room in room_to_creds) {
+      const req = {
+        url: 'https://api.spotify.com/v1/me/player/pause',
+        headers: {
+          'Authorization': 'Bearer ' + room_to_creds[room]["access_token"],
+        },
+        json: true
+      }
+      request.put(req, function(error, response, body) {
+        if(error || response.statusCode != 204) {
+          console.log(error);
+        }
+      })
+    }
+  });
+
+  socket.on('play', function (message) {
+    room = message["room"]
+    if (room in room_to_creds) {
+      const req = {
+        url: 'https://api.spotify.com/v1/me/player/play',
+        headers: {
+          'Authorization': 'Bearer ' + room_to_creds[room]["access_token"],
+        },
+        json: true
+      }
+      request.put(req, function(error, response, body) {
+        if (error || response.statusCode != 204) {
+          console.log(error);
+        }
+      })
+    }
+  });
+
+  socket.on('next', function (message) {
+    room = message["room"]
+    if (room in room_to_queue) {
+      queue = room_to_queue[room]["queue"]
+      if (queue.length > 0) {
+        next_track = queue.shift()
+        console.log(next_track)
+        if(next_track) {
+          const new_song_req = {
+            url: 'https://api.spotify.com/v1/me/player/play',
+            headers: {
+              'Authorization': 'Bearer ' + room_to_creds[room]["access_token"],
+            },
+            json: {
+              "uris": [next_track["uri"]]
+            },
+          }
+
+          request.put(new_song_req, function(error, response, body) {
+            try {
+              if (!error && response.statusCode === 204) {
+                if (room in room_to_queue) {
+                  room_to_queue[room]["currently_playing"] = next_track
+                  console.log(room_to_queue[room])
+                  io.in(room).emit('queue', room_to_queue[room]);
+                }
+              }
+              console.log(response.statusCode)
+            } catch (err) {
+              console.log(err)
+            }
+          })
+        }
+      }
+    }
+  })
+
   socket.on('disconnect', function () {
     console.log('user disconnected');
   });
@@ -83,10 +156,12 @@ setInterval(() => {
             if (!error && response.statusCode === 200) {
               currently_playing_song = body["item"]["uri"];
               progress = body["progress_ms"];
+              is_playing = body["is_playing"]
               if (room_to_queue[room]["currently_playing"] != undefined) {
                 room_to_queue[room]["currently_playing"]["progress"] = progress
+                room_to_queue[room]["currently_playing"]["is_playing"] = is_playing
               }
-              is_not_playing = (body["is_playing"] === false && progress === 0)
+              is_not_playing = (is_playing === false && progress === 0)
               io.in(room).emit('queue', room_to_queue[room])
 
               if (queue.length > 0 && currently_playing_song !== room_to_queue[room]["currently_playing"]["uri"] || is_not_playing) {
--- shared-spotify-queue/src/search.jsx
+++ shared-spotify-queue/src/search.jsx
@@ -4,13 +4,15 @@ import Button from 'react-bootstrap/Button';
 import ProgressBar from 'react-bootstrap/ProgressBar'
 import AsyncSelect from 'react-select/async'
 import socketIOClient from "socket.io-client";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faPause, faForward } from '@fortawesome/free-solid-svg-icons'
 
 class Search extends React.Component {
   state = {
     selectedOptions: [],
     currentSong: "",
     endpoint: process.env.REACT_APP_SOCKET,
-    socket: null
+    socket: null,
   }
 
   componentWillMount() {
@@ -39,7 +41,7 @@ class Search extends React.Component {
       .then(json => {
         console.log(json["tracks"]["items"])
         return json["tracks"]["items"].map((item) => (
-          { value: item["name"], label: item["name"], artist: item["artists"][0]["name"], uri: item["uri"], image: item["album"]["images"][2]["url"], duration: item["duration_ms"], progress: 0 }
+          { value: item["name"], label: item["name"], artist: item["artists"][0]["name"], uri: item["uri"], image: item["album"]["images"][0]["url"], duration: item["duration_ms"], progress: 0, is_playing: true }
         ))
       })
   }
@@ -58,11 +60,36 @@ class Search extends React.Component {
     socket.emit('delete room', message)
   }
 
+  getIcon = () => {
+    return faPlay;
+  }
+
+  playOrPause = () => {
+    const { room } = this.props.match.params
+    const { socket } = this.state;
+    const { currentSong } = this.state
+    
+    var message = {room: room}
+
+    if (currentSong["is_playing"] === false) {
+      socket.emit('play', message)
+    } else {
+      socket.emit('pause', message)
+    }
+  }
+
+  nextSong = () => {
+    const { room } = this.props.match.params
+    const { socket } = this.state;
+
+    var message = {room: room}
+
+    socket.emit('next', message)
+  }
+
   render() {
     const { room } = this.props.match.params
     const { selectedOptions, currentSong } = this.state
-    //console.log(selectedOptions)
-    //console.log(currentSong)
   	return (
       <div className={"flex-container"}>
         <div>Welcome to room: {room}</div>
@@ -70,8 +97,21 @@ class Search extends React.Component {
           <div className={"flex-item"}>
             <img className={"album"} src={currentSong["image"]}></img>
             <div className={"song-info"}>
-              <div>{currentSong["value"]}</div>
-              <div>{currentSong["artist"]}</div>
+              <div className={"player-details"}>
+                <div>
+                  <div>{currentSong["value"]}</div>
+                  <div>{currentSong["artist"]}</div>
+                </div>
+                <div className={"controls"}>
+                  <span className={"play"} onClick={this.playOrPause}>
+                    <FontAwesomeIcon icon={currentSong["is_playing"] ? faPause : faPlay} />
+                  </span>
+                  <span className={"next"} onClick={this.nextSong}>
+                    <FontAwesomeIcon icon={faForward} />
+                  </span>
+                </div>
+              </div>
+
               <div className={"progress-div"}>
                 <ProgressBar variant="info" now={(currentSong["progress"]/currentSong["duration"])*100} />
               </div>
